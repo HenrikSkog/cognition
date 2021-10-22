b5c7d0b22970f9fb443d6413dda911944abed039
Thomas Hasvold Adam <thomhas@stud.ntnu.no>
2021-10-22 15:54:15 +0000

    Feat/#49 dashboard content

--- cognition/ui/src/main/java/ui/controllers/DashboardController.java
+++ cognition/ui/src/main/java/ui/controllers/DashboardController.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import json.CognitionStorage;
 
@@ -24,9 +25,23 @@ public class DashboardController extends LoggedInController {
     super(user, cognitionStorage);
   }
 
+
+  /**
+   * Adds application description to dashboard.
+   */
+
   @FXML
   public void initialize() {
     heading.setText("Welcome, " + Tools.capitalize(getUser().getUsername()));
-    cognitionDescription.setText("TODO: Add some descriptive text here.");
+
+    
+    cognitionDescription.setText("Cognition is an interactive game-based "
+            + "learning tool used to study information. This tool uses interactive "
+            + "digital flashcards. Teachers and Students can easily upload information "
+            + "to create study sets by adding questions and definitions. This tool is "
+            + "easy to navigate and provides a fun and interactive playground to the "
+            + "learners as they engage with the application.");
+    cognitionDescription.setFont(Font.font("Avenir book", 25));
+
   }
 }
