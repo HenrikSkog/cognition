3c3ceeb356251212c0bd97d5c584e8641f5e40f7
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-11-23 00:15:54 +0100

    feat: Add working integration test
    Issue: #101

--- cognition/integration_tests/src/test/java/integration_tests/AppTest.java
+++ cognition/integration_tests/src/test/java/integration_tests/AppTest.java
@@ -1,6 +1,7 @@
 package integration_tests;
 
 import api.RestApplication;
+import core.User;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.*;
 import org.testfx.framework.junit5.ApplicationTest;
 import ui.LoginController;
 import ui.RemoteCognitionAccess;
+import java.io.IOException;
 
 public class AppTest extends ApplicationTest {
   private LoginController loginController;
@@ -18,11 +20,9 @@ public class AppTest extends ApplicationTest {
 
   @BeforeAll
   static void beforeAll() {
-    // Ensure that we're using file for testing in persistent storage
+    // Ensure that we're using file for testing in persistent storage, and start web server
     RestApplication.setTestMode(true);
-
-    // Start web server
-    RestApplication.start();
+    RestApplication.main(new String[]{});
   }
 
   @AfterAll
@@ -42,7 +42,7 @@ public class AppTest extends ApplicationTest {
 
     // Set state in controller.
     // The controller uses a RemoteCognitionAccess configured with the port for testing
-    this.loginController = new LoginController(new RemoteCognitionAccess(RestApplication.TEST_PORT));
+    this.loginController = new LoginController(new RemoteCognitionAccess());
     loader.setController(loginController);
 
     // Switch stage
@@ -57,10 +57,19 @@ public class AppTest extends ApplicationTest {
     Assertions.assertNotNull(loginController);
   }
 
-  // @Test
-  // @DisplayName("Client can connect to web server.")
-  // void clientCanConnectToWebServer() {
-  //   User user = new User(validUsername, validPassword);
-  //   this.loginController.getRemoteCognitionAccess().create(user);
-  // }
+  @Test
+  @DisplayName("Client can connect to web server.")
+  void clientCanConnectToWebServer() throws IOException, InterruptedException {
+    User user = new User(validUsername, validPassword);
+
+    // Create sample user
+    this.loginController.getRemoteCognitionAccess().create(user);
+
+    // Read stored user
+    User parsedUser = loginController.getRemoteCognitionAccess().read(validUsername);
+
+    // Assertions
+    Assertions.assertNotNull(parsedUser);
+    Assertions.assertEquals(validUsername, parsedUser.getUsername());
+  }
 }
