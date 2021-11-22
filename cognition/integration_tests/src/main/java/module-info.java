1db18251e4d0475325bb5046705129a37ab9021c
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-11-22 23:49:08 +0100

    deps: Update dependencies
    Issue: #101

--- cognition/integration_tests/src/main/java/module-info.java
+++ cognition/integration_tests/src/main/java/module-info.java
@@ -6,6 +6,14 @@ module integration_tests {
   requires javafx.controls;
   requires javafx.fxml;
 
+  requires spring.web;
+  requires spring.boot;
+  requires spring.boot.autoconfigure;
+
+  requires spring.data.rest.core;
+  requires spring.data.commons;
+  requires spring.context;
+
   requires transitive com.google.gson;
   requires java.net.http;
 
