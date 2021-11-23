fdde34924f246204f0bde4bb80adb1a879b64405
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-11-23 15:52:15 +0100

    test: Add more robust test to improve test coverage of UI testing in isolation
    Issue: #100

--- cognition/ui/src/test/java/ui/UpdateQuizTest.java
+++ cognition/ui/src/test/java/ui/UpdateQuizTest.java
@@ -1,10 +1,14 @@
 package ui;
 
+import core.Flashcard;
 import core.Quiz;
 import core.User;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -14,6 +18,7 @@ import org.mockito.Mockito;
 import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.matcher.control.LabeledMatchers;
+import java.util.List;
 
 import static core.tools.Tools.createUuid;
 
@@ -41,6 +46,13 @@ public class UpdateQuizTest extends ApplicationTest {
     // logged-in user. Thus, we create a fake user here to emulate it
     User loggedInUser = new User(validUsername, validPassword);
     Quiz quiz = new Quiz(createUuid(), validQuizName, validQuizDescription);
+
+    // Add sample flashcards
+    quiz.addFlashcards(List.of(
+            new Flashcard(createUuid(), "front-1", "answer-1"),
+            new Flashcard(createUuid(), "front-2", "answer-2")
+    ));
+
     loggedInUser.addQuiz(quiz);
 
 
@@ -72,10 +84,37 @@ public class UpdateQuizTest extends ApplicationTest {
   @Test
   @DisplayName("Cannot create empty quiz.")
   void cannotCreateEmpty() {
+    // Setup test by removing initially rendered flashcard nodes
+    removeFlashcardNode();
+    removeFlashcardNode();
+
     TestFxHelper.waitForFxEvents();
     clickOn("#storeQuizButton");
     TestFxHelper.waitForFxEvents();
 
     FxAssert.verifyThat("#feedback", LabeledMatchers.hasText("There has to be at least one flashcard"));
+  }
+
+  @Test
+  @DisplayName("Can remove flashcard.")
+  void canRemoveFlashcard() {
+    // Find container to later check the number of flashcard nodes
+    ScrollPane scrollPane = lookup("#flashcardPane").query();
+    VBox container = (VBox) scrollPane.getContent();
+
+    boolean renderedNumberOfFlashcardNodesCorrectly = container.getChildren().size() == 2;
+    Assertions.assertTrue(renderedNumberOfFlashcardNodesCorrectly);
+
+    removeFlashcardNode();
+
+    boolean removedFlashcardNode = container.getChildren().size() == 1;
+    Assertions.assertTrue(removedFlashcardNode);
+  }
+
+  /**
+   * Helper method for removing a flashcard node in the view.
+   */
+  private void removeFlashcardNode() {
+    clickOn((Button) lookup("#remove-flashcard-button").query());
   }
 }
