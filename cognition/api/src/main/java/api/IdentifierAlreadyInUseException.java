62a6877b7e700ae7041d2419ca54289df916d997
Thomhas <thomas.hasvold@gmail.com>
2021-11-24 21:10:24 +0100

    feat: Add core and api diagrams
    WHAT: Adds 2 diagrams for core (a class diagram and a descriptive diagram) as well as the 2 diagrams for API (a class diagram and a descriptive diagram)
    
    WHY: Visual representation of the connections between our modules is inportant for current members of the team and very important for other developers that would want to contributeto the project
    
    Reference: #97

--- cognition/api/src/main/java/api/IdentifierAlreadyInUseException.java
+++ cognition/api/src/main/java/api/IdentifierAlreadyInUseException.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  */
 @ResponseStatus(value = HttpStatus.CONFLICT, reason = "Identifier already in use")
 public class IdentifierAlreadyInUseException extends RuntimeException {
-  
-  public IdentifierAlreadyInUseException(String identifier) {
-    super("The identifier: " + identifier + " is already in use.");
-  }
 
-  public IdentifierAlreadyInUseException() {
-    super("Identifier already in use.");
-  }
+    public IdentifierAlreadyInUseException(String identifier) {
+        super("The identifier: " + identifier + " is already in use.");
+    }
+
+    public IdentifierAlreadyInUseException() {
+        super("Identifier already in use.");
+    }
 }
